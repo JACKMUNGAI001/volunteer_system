c4d03f7c9dbb966faa6d1019a18fd0811dbc5066
Jackson <jacksonmungai001@gmail.com>
2025-08-27 23:07:29 +0300

    Add Event model in model.py

--- model.py
+++ model.py
@@ -1,5 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Date, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+class Event(Base):
+    __tablename__ = 'events'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    date = Column(Date, nullable=False)
+    location = Column(String, nullable=False)
+    required_skills = Column(String)
+    assignments = relationship("Assignment", back_populates="event")
+
+
 engine = create_engine('sqlite:///volunteer_system.db')
